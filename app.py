3ed37baa0ff65206d57432e0e4877319a852e956
annaweeks <aoweeks@yahoo.com>
2021-12-02 00:11:51 -0800

    updates

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ session = Session(bind=engine)
 
 # Save reference to the table
 AQI_ref = Base.classes.aqi
+AQI_map = Base.classes.map
 
 
 
@@ -56,20 +57,49 @@ def index():
 @app.route("/jsondata")
 def jsondata():
 
-    d = {}
+    d2 = {}
+    d2['aqiData'] = []
+    
 
     year = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
+    d2['year'] = year
+
     for y in year:
+        d = {}
         results = session.query(func.avg(AQI_ref.no2_aqi), func.avg(AQI_ref.o3_aqi), func.avg(AQI_ref.so2_aqi), func.avg(AQI_ref.co_aqi))\
             .filter(extract('year', AQI_ref.date) == y)\
             .filter(AQI_ref.county == 'San Diego')\
             .filter(AQI_ref.state =='California').all()
-        d[y] = list(results[0])
-
-    print(d)
-    rs = json.dumps(d)
-
+        d['year'] = y
+        d['aqi'] = list(results[0])
+        d2['aqiData'] .append(d)
+    
+    rs = json.dumps(d2)
+    print('_______________________________')
+    print(rs)
+    print('_______________________________')
     return rs
+
+@app.route("/map")
+def map():
+
+    d2 = {}
+
+    years = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016']
+    d2['year'] = years
+
+    for y in years:
+        result = session.query(AQI_map.state, AQI_map.no2_aqi, AQI_map.o3_aqi, AQI_map.so2_aqi, AQI_map.co_aqi)\
+            .filter(extract('year', AQI_map.date) == y).all()
+        print(result)
+    
+    rs = json.dumps(d)
+    print('_______________________________')
+    print(rs)
+    print('_______________________________')
+    return rs
+
+    
 
 
 if __name__ == "__main__":
